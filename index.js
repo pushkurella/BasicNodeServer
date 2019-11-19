449d376df352d867e03c23c560621152ec8c8d2f
Pushpak <pushpak.kurella7@gmail.com>
2019-11-18 22:46:19 -0500

    chapter 2 node using express

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
-const http = require('http');
-const fs = require('fs');
-const loginPage = fs.readFileSync('loginpage.html');
-const contactPage = fs.readFileSync('contact.html');
-const homePage = fs.readFileSync('homepage.html');
-const error = fs.readFileSync('error.html');
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static('public'));
 
-const server = http.createServer((req,res) => {
-    console.log('url is '+ req.url);
-    let a = req.url;
-   
-    if(req.url === '/login'){
-        res.end(loginPage);
-    }
-    else if(req.url === '/home'){
-        res.end(homePage);
-    }
-    else if(req.url === '/contact'){
-        res.end(contactPage);
-    }
-    else if(req.url === '/favicon.ico'){
-        res.end('favicon');
-    }
-    else{
-        console.log('else block')
-        res.writeHead(404);
-        res.end(error);
-    }
-    //res.end('welcome to node js');
+app.listen(3000,()=>{
+    console.log('app running on 3000');
+})
+
+app.get('/', (req, res)=> {
+    res.json({
+        message: 'hello world'
+    });
 });
-server.listen(3000);
+
+app.get('/index',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'index.html'));
+})
+
+app.get('/contact', (req,res)=>{
+    res.sendFile(path.resolve(__dirname),'contact.html');
+});
+app.get('/error', (req,res)=>{
+    res.sendFile(path.resolve(__dirname),'error.html');
+});
